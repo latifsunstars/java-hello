90d9b7890a4683771b6e837ff19a17805ec68a64
latifsunstars <32881755+latifsunstars@users.noreply.github.com>
2019-01-16 11:09:22 +0530

    Add files via upload

---
+++ HelloJava/src/hellojava/HelloJava.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hellojava;
+
+/**
+ *
+ * @author student1
+ */
+public class HelloJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      System.out.println("Hello!Java\nThis is my 1st project");  
+// TODO code application logic here
+    }
+    
+}
